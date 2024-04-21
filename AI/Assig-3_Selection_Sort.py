baeb43472a92f3ff543332a72367eebbcf2f9a3b
Yashhh03 <yashsurya8888@gmail.com>
2024-04-21 19:01:40 +0530

    made some changes

--- AI/Assig-3_Selection_Sort.py
+++ AI/Assig-3_Selection_Sort.py
@@ -4,12 +4,12 @@ def selectionSort(array, size):
         for j in range(ind + 1, size):
             if array[j] < array[min_index]:
                 min_index = j
-        (array[ind], array[min_index]) = (array[min_index], array[ind])
+        array[ind], array[min_index] = array[min_index], array[ind]
 
-arr = list(map(int, input("Enter the array elements separated by space: ").split()))
+arr = input("Enter the elements separated by space: ").split()
 
 size = len(arr)
 selectionSort(arr, size)
 
 print('The array after sorting in Ascending Order by selection sort is:')
-print(arr)
+print(' '.join(arr))
